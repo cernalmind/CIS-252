eaf459a2a8673c53f2f6c47d3b4621d5670c553d
Patrick Brown <patrick.arthur.brown@gmail.com>
2013-09-03 21:39:29 -0400

    number guess

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ CIS-252
 =======
 
 Course work for Prof. Anderson's C++ Programming
+
+
+Index
+-----
+
+###Chapter 1
+- number_guess.cpp
---
+++ number_guess.cpp
@@ -0,0 +1,42 @@
+/*-------------------------------------------------
+Author:    Patrick Brown
+Date:      2013-09-03
+Objective: Simple game where player guesses a
+           random number and the computer tells
+           them if their guess was too low or too
+           high.
+-------------------------------------------------*/
+#include <iostream>
+#include <stdlib.h>
+
+
+int main()
+{
+    int number;
+    int guess;
+
+    // random seed
+    srand(time(NULL));
+
+    number = rand() % 100 + 1;
+
+    std::cout << "I'm thinking of a number between 1 and 100." << std::endl;
+
+    while(guess != number)
+    {
+
+        std::cout << "Enter your guess: ";
+        std::cin >> guess;
+
+        if(guess < number)
+        {
+            std::cout << "Too low!" << std::endl;
+        } else if(guess > number) {
+            std::cout << "Too high!" << std::endl;
+        }
+    }
+
+    std::cout << "Congrats: You win!" << std::endl;
+
+    return 0;
+}
