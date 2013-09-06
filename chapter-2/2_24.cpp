8adaf8a4d06f43509170479cd6ad1cc7d28cff59
Patrick Brown <patrick.arthur.brown@gmail.com>
2013-09-06 13:03:56 -0400

    2.24 Odd/Even

---
+++ chapter-2/2_24.cpp
@@ -0,0 +1,24 @@
+/*------------------------------------------------------------------
+Author:    Patrick Brown
+Date:      2013-09-06
+Objective: A program that reads an integer and determines and prints
+           whether it’s odd or even. 
+------------------------------------------------------------------*/
+#import <iostream>
+
+using namespace std;
+
+int main()
+{
+  int input;
+
+  cout << "Enter an integer and I'll tell you if it's odd or even." << endl;
+  cout << "> ";
+  cin >> input;
+
+  if(input%2 == 0){
+    cout << input << " is an even number." << endl;
+  } else {
+    cout << input << " is an odd number." << endl;
+  }
+}
