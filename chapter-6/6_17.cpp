321d995581aa026c23f089045b23d103872b77e7
Patrick Brown <patrick.arthur.brown@gmail.com>
2013-10-27 19:19:43 -0400

    6.17 Random Numbers

---
+++ chapter-6/6_17.cpp
@@ -0,0 +1,35 @@
+/**
+ * 6_17.cpp - (Random Numbers)
+ *
+ * Write a single statement that prints a number at random from each
+ * of the following sets:
+ *     a) 2, 4, 6, 8, 10.
+ *     b) 3, 5, 7, 9, 11.
+ *     c) 6, 10, 14, 18, 22.
+ */
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h> 
+
+using namespace std;
+
+int main()
+{
+    int output;
+
+    /* initialize random seed: */
+    srand (time(NULL));
+
+    // a) 2, 4, 6, 8, 10.
+    output = ((rand() % 5) * 2) + 2;
+    cout << output << endl;
+
+    // b) 3, 5, 7, 9, 11.
+    output = ((rand() % 5) * 2) + 3;
+    cout << output << endl;
+
+    // c) 6, 10, 14, 18, 22.
+    output = ((rand() % 5) * 4) + 6;
+    cout << output << endl;
+}
